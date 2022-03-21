67ed677a72293402105e52d8a96cb10144ab3fa5
rmn1211 <roman.blasen@gmail.com>
2022-03-21 20:49:50 +0100

    HTTP Request an API funktioniert
    Bilder können an API gesendet werden ohne dass diese zwischengespeicvhert werden müssen

--- proto/app/Http/Controllers/ImageUploadController.php
+++ proto/app/Http/Controllers/ImageUploadController.php
@@ -1,80 +1,43 @@
 <?php
-
-  
-
 namespace App\Http\Controllers;
-
-  
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-  
 
 class ImageUploadController extends Controller
-
 {
-
-     /**
-
+    /**
      * Display a listing of the resource.
-
      *
-
      * @return \Illuminate\Http\Response
-
      */
 
-   
-    
-
     /**
-
      * Display a listing of the resource.
-
      *
-
      * @return \Illuminate\Http\Response
-
      */
 
     public function imageUploadPost(Request $request)
-
     {
-
-        $request->validate([
-
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-
-        ]);
-
-    
-
-        $imageName = time().'.'.$request->image->extension();  
-
-     
-//$request->image->storeAs('images', $imageName);// storage/app/images/file.png  aktueller ort
-
-  //$request->image-> neue request nur mit image
-  $response = Http::attach(     
-    'attachment', file_get_contents($request->image)
-)->post('http://deineApi');
-  //nächste Blade
-
-
-  
+        /*$request->validate([
+        'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);*/
+        $imageName = time() . '.' . $request->image->extension();
+        //$request->image->storeAs('images', $imageName);// storage/app/images/file.png  aktueller ort
+        //$request->image-> neue request nur mit image
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $request = Http::attach(
+                'file', file_get_contents($image), $imageName)
+                ->post('http://192.168.1.168:8000/upload/1');
+        }
+        //nächste Blade
 
         /* Store $imageName name in DATABASE from HERE */
-
-    
-
         return view('report.controll_handwriting');
-
         //back()
-
-          //  ->with('success','You have successfully upload image.')
-
-            //->with('image',$imageName); 
-
+        //  ->with('success','You have successfully upload image.')
+        //->with('image',$imageName);
     }
-
 }
--- proto/resources/views/report/upload.blade.php
+++ proto/resources/views/report/upload.blade.php
@@ -7,10 +7,10 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta charset="utf-8">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-       <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-      
+
         <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
         <style>
@@ -18,51 +18,51 @@
             body {
                 height: 100%;
             }
+
         </style>
 
     </head>
     <br>
     <br>
 
-     <section class="flex  justify-center items-center">    <br>
+    <section class="flex  justify-center items-center"> <br>
 
         <h3 class=" font-bold  text-2xl">Spielbericht hochladen</h3>
-       
+
     </section>
-    <br>    <br>
+    <br> <br>
     <br>
 
-     <section  class="flex  justify-center items-center ">
- <div>
-     <form action="{{ route('image.upload.post') }}" method="POST" enctype="multipart/form-data">
+    <section class="flex  justify-center items-center ">
+        <div>
+            <form action="{{ route('image.upload.post') }}" method="POST" enctype="multipart/form-data">
 
-            @csrf
+                @csrf
 
-            
 
-    
 
-               <div class="row">
+
+
+                <div class="row">
 
                     <input type="file" name="image" class="form-control bg-green-500 disabled:opacity-90 hover:bg-green-700 text-white font-bold py-2 px-4 mx-8 border-green-700 rounded">
 
-              
-     
 
-               
+
+
+
 
                     <button type="submit" class="btn btn-success bg-green-500 disabled:opacity-90 hover:bg-green-700 text-white font-bold py-2 px-4 mx-8 border-green-700 rounded">Bild hochladen</button>
 
-              
+
                     <!-- warten auf Antwort,  Bericht mit A ausfüllen  -->
-     
 
-          
 
-        </form>
-      
-         <button   class="bg-green-500 disabled:opacity-90 hover:bg-green-700 text-white font-bold py-2 px-4 m-8 border-green-700 rounded"type="button"  onclick="window.location='{{ url('/make_report') }}'">Bericht ausfüllen </button> </div>
-         </section>
 
-              
-    @endsection
+
+            </form>
+
+            <button class="bg-green-500 disabled:opacity-90 hover:bg-green-700 text-white font-bold py-2 px-4 m-8 border-green-700 rounded" type="button" onclick="window.location='{{ url('/make_report') }}'">Bericht ausfüllen </button>
+        </div>
+    </section>
+@endsection
